36e2d9aa94a382014d0992a6ea1b6f2b55753cf5
ZoezTV <70135391+ZoezTV@users.noreply.github.com>
2022-12-05 11:00:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "DSU ist toll";
+echo "<html>";
+echo <iframe src="https://giphy.com/embed/Wrh8aL75aj4uZwuqta" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/stickbug-stick-bug-get-stickbugged-Wrh8aL75aj4uZwuqta">via GIPHY</a></p>
 ?>
-<html>
-<iframe src="https://giphy.com/embed/Wrh8aL75aj4uZwuqta" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/stickbug-stick-bug-get-stickbugged-Wrh8aL75aj4uZwuqta">via GIPHY</a></p>
-</html>
+
